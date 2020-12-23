f86dc76c0f141383ca2f55845c1127b08b153f21
julia_chizhik <for.yu.ch@gmail.com>
2020-12-23 12:21:05 +0200

    update

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+
+$(function () {
+        $(document).on('click touchstart', '.category-card', function(){ 
+            console.log($(this));
+            console.log('card click');
+        });
+    });
+
