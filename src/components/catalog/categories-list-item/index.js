3c59cee61434b839c1d4590b2d70e687b771f2d6
julia_chizhik <for.yu.ch@gmail.com>
2020-12-23 10:37:28 +0200

    working

--- src/components/catalog/categories-list-item/index.js
+++ src/components/catalog/categories-list-item/index.js
@@ -1,6 +1,7 @@
-// import { add } from 'lodash';
 import './styles.css';
 import categoryTemplate from './template/category.hbs';
+
+const categoryRefs = document.querySelectorAll('.category-list');
 
 
 $(window).on( 'load', function () {
@@ -46,27 +47,110 @@ $(window).on('load', function() {
     console.log( "ready!" );
 });
 
+// проба загрузки=========================================================
 
-let addData = [];
-// const liImgRef = document.querySelector('.category-js');
-const ulImgRef = document.querySelectorAll('.category-list');
+// let addData = [];
 
-console.log(ulImgRef);
+// const ulImgRef = document.querySelectorAll('.category-list');
 
-const getCall = async () => { 
-    return await fetch('https://callboard-backend.herokuapp.com/call/ads')
+// console.log(ulImgRef);
+
+// const getCall = async () => { 
+//     return await fetch('https://callboard-backend.herokuapp.com/call/ads')
+//         .then(response => response.json())
+//         .then(data => { addData = [...data]; console.log(addData); })
+// }
+
+// const createCategory = async () => {
+//     await getCall();
+//     console.log(addData);
+//     const markup = categoryTemplate(addData);
+//     ulImgRef[0].insertAdjacentHTML('afterbegin', markup);
+//     ulImgRef[1].insertAdjacentHTML('afterbegin', markup);
+//     ulImgRef[2].insertAdjacentHTML('afterbegin', markup);
+//     console.log('markup ready');
+// }
+// createCategory();
+
+// ===================================================================
+
+let addCategoryProperty;
+
+const getCallСategoryProperty = async () => { 
+    return await fetch('https://callboard-backend.herokuapp.com/call/specific/property')
         .then(response => response.json())
-        .then(data => addData = [...data])
+        .then(data => {
+            // console.log(data);
+            addCategoryProperty = [...data];
+        })
 }
 
-const createCategory = async () => {
-    await getCall();
-    console.log(addData);
-    const markup = categoryTemplate(addData);
-    ulImgRef[0].insertAdjacentHTML('afterbegin', markup);
-    ulImgRef[1].insertAdjacentHTML('afterbegin', markup);
-    ulImgRef[2].insertAdjacentHTML('afterbegin', markup);
-    console.log('markup ready');
+const createСategoryProperty = async () => {
+    await getCallСategoryProperty();
+    console.log(addCategoryProperty);
+    const markup = categoryTemplate(addCategoryProperty);
+    categoryRefs[1].insertAdjacentHTML('afterbegin', markup);
+    console.log('markup category ready Property');
 }
-createCategory();
+createСategoryProperty();
 
+let addCategoryTransport;
+
+const getCallСategoryTransport = async () => { 
+    return await fetch('https://callboard-backend.herokuapp.com/call/specific/transport')
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            addCategoryTransport = [...data];
+        })
+}
+
+const createСategoryTransport = async () => {
+    await getCallСategoryTransport();
+    console.log(addCategoryTransport);
+    const markup = categoryTemplate(addCategoryTransport);
+    categoryRefs[2].insertAdjacentHTML('afterbegin', markup);
+    console.log('markup category ready Transport');
+}
+createСategoryTransport();
+
+
+let addCategoryWork;
+
+const getCallСategoryWork = async () => { 
+    return await fetch('https://callboard-backend.herokuapp.com/call/specific/work')
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            addCategoryWork = [...data];
+        })
+}
+
+const createСategoryWork = async () => {
+    await getCallСategoryWork();
+    console.log(addCategoryWork);
+    const markup = categoryTemplate(addCategoryWork);
+    categoryRefs[3].insertAdjacentHTML('afterbegin', markup);
+    console.log('markup category ready Work');
+}
+createСategoryWork();
+
+// let addCategoryWork;
+
+// const getCallСategoryWork = async () => { 
+//     return await fetch('https://callboard-backend.herokuapp.com/call/specific/work')
+//         .then(response => response.json())
+//         .then(data => {
+//             // console.log(data);
+//             addCategoryWork = [...data];
+//         })
+// }
+
+// const createСategoryWork = async () => {
+//     await getCallСategoryWork();
+//     console.log(addCategoryWork);
+//     const markup = categoryTemplate(addCategoryWork);
+//     categoryRefs[3].insertAdjacentHTML('afterbegin', markup);
+//     console.log('markup category ready Work');
+// }
+// createСategoryWork();
