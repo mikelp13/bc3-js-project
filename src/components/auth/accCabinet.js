3d963cc3fcbbae76ec3090bcfc6e74bfc9493ee8
ka2nq1 <ka2nq1@gmail.com>
2020-12-23 13:15:17 +0200

    compl

--- src/components/auth/accCabinet.js
+++ src/components/auth/accCabinet.js
@@ -1,14 +1,12 @@
 import accCabinet from './template/accCabinet.hbs';
 import {data} from '../../data/data';
-const loggedUserCarts = document.querySelector('#loggedUser__carts');
 const authWrapper = document.querySelector('.header-auth')
-const headerUpperWrapper = document.querySelector('.desktop-upper-wrapper')
-
-
+const authMobileWrapper = document.querySelector('.header-auth-mobile')
 
 function CreateCabinetMarkup() {
     authWrapper.insertAdjacentHTML('beforebegin', accCabinet())
+    authMobileWrapper.insertAdjacentHTML('afterbegin', accCabinet())
+    
 }
 
 CreateCabinetMarkup()
-// console.log(authWrapper.innerHTML = accCabinet());
