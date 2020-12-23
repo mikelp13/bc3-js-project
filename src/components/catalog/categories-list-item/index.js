b7dd89d570592bf552a7d596279a7488a24751dc
julia_chizhik <for.yu.ch@gmail.com>
2020-12-23 05:58:00 +0200

    working

--- src/components/catalog/categories-list-item/index.js
+++ src/components/catalog/categories-list-item/index.js
@@ -1,4 +1,4 @@
-import { add } from 'lodash';
+// import { add } from 'lodash';
 import './styles.css';
 import categoryTemplate from './template/category.hbs';
 
@@ -12,8 +12,8 @@ $(window).on( 'load', function () {
         speed: 500,
         easing: 'ease',
         infinite: true,
-        // autoplay: true,
-        // autoplaySpeed: 1500,
+        autoplay: true,
+        autoplaySpeed: 3500,
         
         responsive: [
             {
@@ -47,74 +47,26 @@ $(window).on('load', function() {
 });
 
 
-// import itemsTemplate from '../templates/gallery-items.hbs'; +
-// import menuItems from '../menu.json';
-
-// const markup = itemsTemplate(menuItems);
-// const menuRef = document.querySelector('.js-menu');
-// menuRef.insertAdjacentHTML('beforeend', markup);
-
-// let jsonData = {};
-// function createCategory(categories) { 
-//     const markup = categoryTemplate(categories);
-//     refs.categories.in
-// }
-
-// let addData = [];
+let addData = [];
 // const liImgRef = document.querySelector('.category-js');
+const ulImgRef = document.querySelectorAll('.category-list');
 
-// const getCall = async () => { 
-//     return await fetch('https://callboard-backend.herokuapp.com/call/ads')
-//         .then(response => response.json())
-//         .then(data => addData = [...data])
-// }
+console.log(ulImgRef);
 
+const getCall = async () => { 
+    return await fetch('https://callboard-backend.herokuapp.com/call/ads')
+        .then(response => response.json())
+        .then(data => addData = [...data])
+}
 
-
-// 1 вариант =============================================================
-
-// let addData = [];
-// const liImgRef = document.querySelector('.category-js');
-
-// const getCall = async () => { 
-//     return await fetch('https://callboard-backend.herokuapp.com/call/ads')
-//         .then(response => response.json())
-//         .then(data => addData = [...data])
-// }
-
-// const createImg = (data) => { 
-
-//     return `<li class="category-card sliders-item">
-//               <img class="category-card__img" src=${data.imageUrl} alt="photo ${data.title}">
-//               <p class="category-card__name">${data.title}</p>
-//               <div class="category-card__prices">
-//                 <p class="category-card__price--new">${data.price}</p>
-//               </div>
-//             </li>`
-// }
-
-
-// const createMarkup = async () => { 
-//     await getCall();
-//     console.log('console result', typeof addData, addData);
-//     addData.forEach((data) => {
-//         console.log(typeof data, data);
-//         // liImgRef.innerHTML = createImg(data);
-//     });
-//     // let i = 0;
-//     // if (i < addData.length) { 
-//     //     console.log('before', i);
-//     //     liImgRef.innerHTML = createImg(addData[i]);
-//     //     i += 1;
-//     //     console.log("after", i);
-//     // }
-//     liImgRef.innerHTML = createImg(addData[0]);
-//     console.log('markup ready!');
-
-// }
-// createMarkup();
-
-// =============================================================================
-
-
+const createCategory = async () => {
+    await getCall();
+    console.log(addData);
+    const markup = categoryTemplate(addData);
+    ulImgRef[0].insertAdjacentHTML('afterbegin', markup);
+    ulImgRef[1].insertAdjacentHTML('afterbegin', markup);
+    ulImgRef[2].insertAdjacentHTML('afterbegin', markup);
+    console.log('markup ready');
+}
+createCategory();
 
